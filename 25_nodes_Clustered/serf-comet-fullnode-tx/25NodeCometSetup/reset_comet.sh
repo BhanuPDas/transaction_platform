ea833a8c6a508b5e1cd3e9cde4dfbac60e9ff11e
Bhanu Pratap Das <bhanupd199@gmail.com>
2026-03-12 21:40:40 +0100

    Adding relayer changes

--- 25_nodes_Clustered/serf-comet-fullnode-tx/25NodeCometSetup/reset_comet.sh
+++ 25_nodes_Clustered/serf-comet-fullnode-tx/25NodeCometSetup/reset_comet.sh
@@ -43,7 +43,7 @@ reset_cometbft() {
     docker exec "$container" bash -c "cd /root && rm -rf abci && mkdir -p abci"
     docker cp "./abci/." "$container":/root/abci/ || { echo "Failed to copy abci files to $container"; exit 1; }
     docker exec "$container" bash -c "cd /root/abci && /usr/local/go/bin/go clean -modcache && /usr/local/go/bin/go mod tidy && /usr/local/go/bin/go build -o /root/abci-app *.go"
-    if (( i <= 12 )); then
+    if (( i < 12 )); then
       docker exec -d "$container" bash -c "cd /root/abci/clusterAConfig && nohup /root/abci-app > /root/logs/abci.log 2>&1"
     else
       docker exec -d "$container" bash -c "cd /root/abci/clusterBConfig && nohup /root/abci-app > /root/logs/abci.log 2>&1"
@@ -52,7 +52,7 @@ reset_cometbft() {
 
     echo "[6] Restarting CometBFT..."
     docker exec "$container" rm -f /root/.cometbft/config/genesis.json
-    if (( i <= 12 )); then
+    if (( i < 12 )); then
       docker cp "./cluster1Config/genesis.json" "$container":/root/.cometbft/config/
     else
       docker cp "./cluster2Config/genesis.json" "$container":/root/.cometbft/config/
