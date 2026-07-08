5ade92c205e13e3a0100da66e72f1e8730136bb7
Bhanu Pratap Das <bhanupd199@gmail.com>
2026-07-08 10:16:57 +0200

    25 nodes changes

--- 25_nodes_Clustered/serf-comet-fullnode-tx/25NodeCometSetup/reset_comet.sh
+++ 25_nodes_Clustered/serf-comet-fullnode-tx/25NodeCometSetup/reset_comet.sh
@@ -18,24 +18,41 @@ reset_cometbft() {
     echo "=============================================="
     echo "Resetting ABCI + CometBFT on $container..."
     echo "=============================================="
-
-    echo "[1] Killing CometBFT..."
-    comet_pid=$(docker exec "$container" pgrep -f "/root/go/bin/cometbft node")
-    if [[ -n "$comet_pid" ]]; then
-      docker exec "$container" kill -9 $comet_pid
+    echo "[1] Killing Kafka Tx Producer..."
+    tx_pid=$(docker exec "$container" pgrep -f "python3 tx_producer.py")
+    if [[ -n "$tx_pid" ]]; then
+      docker exec "$container" kill -9 $tx_pid
       sleep 1
     else
-      echo "CometBFT not running"
+      echo "Python Tx Producer not running"
     fi
 
-    echo "[2] Killing ABCI..."
-    abci_pid=$(docker exec "$container" pgrep -f "/root/abci-app")
-    if [[ -n "$abci_pid" ]]; then
-      docker exec "$container" kill -9 $abci_pid
+    echo "[2] Killing Kafka Tx Consumer..."
+    tx_pid=$(docker exec "$container" pgrep -f "python3 tx_consumer.py")
+    if [[ -n "$tx_pid" ]]; then
+      docker exec "$container" kill -9 $tx_pid
       sleep 1
     else
-      echo "ABCI not running"
+      echo "Python Tx Consumer not running"
     fi
+
+#    echo "[1] Killing CometBFT..."
+#    comet_pid=$(docker exec "$container" pgrep -f "/root/go/bin/cometbft node")
+#    if [[ -n "$comet_pid" ]]; then
+#      docker exec "$container" kill -9 $comet_pid
+#      sleep 1
+#    else
+#      echo "CometBFT not running"
+#    fi
+
+#    echo "[2] Killing ABCI..."
+#    abci_pid=$(docker exec "$container" pgrep -f "/root/abci-app")
+#    if [[ -n "$abci_pid" ]]; then
+#      docker exec "$container" kill -9 $abci_pid
+#      sleep 1
+#    else
+#      echo "ABCI not running"
+#    fi
     echo "[3] Killing Tx API..."
     tx_pid=$(docker exec "$container" pgrep -f "python3 tx_api.py")
     if [[ -n "$tx_pid" ]]; then
@@ -45,37 +62,37 @@ reset_cometbft() {
       echo "Python Tx API not running"
     fi
 
-    echo "[4] Removing state.db..."
-    docker exec "$container" rm -rf /root/abci/state.db
-    sleep 1
+#    echo "[4] Removing state.db..."
+#    docker exec "$container" rm -rf /root/abci/state.db
+#    sleep 1
 
-    echo "[5] Resetting CometBFT state..."
-    docker exec "$container" /root/go/bin/cometbft unsafe-reset-all
-    sleep 1
+#    echo "[5] Resetting CometBFT state..."
+#    docker exec "$container" /root/go/bin/cometbft unsafe-reset-all
+#    sleep 1
 
-    echo "[6] Restarting ABCI..."
-    docker exec "$container" bash -c "cd /root && rm -rf abci && mkdir -p abci && rm -rf cometclient && mkdir -p cometclient"
-    docker cp "./abci/." "$container":/root/abci/ || { echo "Failed to copy abci files to $container"; exit 1; }
-    docker cp "./cometclient/." "$container":/root/cometclient/ || { echo "Failed to copy main.py file to $container"; exit 1; }
-    docker exec "$container" bash -c "cd /root/abci && /usr/local/go/bin/go clean -modcache && /usr/local/go/bin/go mod tidy && /usr/local/go/bin/go build -o /root/abci-app *.go"
-    if (( i < 12 )); then
-     docker exec -d "$container" bash -c "cd /root/abci/clusterAConfig && nohup /root/abci-app > /root/logs/abci.log 2>&1"
-    else
-      docker exec -d "$container" bash -c "cd /root/abci/clusterBConfig && nohup /root/abci-app > /root/logs/abci.log 2>&1"
-    fi
-    sleep 2
+#    echo "[6] Restarting ABCI..."
+#    docker exec "$container" bash -c "cd /root && rm -rf abci && mkdir -p abci && rm -rf cometclient && mkdir -p cometclient"
+#    docker cp "./abci/." "$container":/root/abci/ || { echo "Failed to copy abci files to $container"; exit 1; }
+#    docker cp "./cometclient/." "$container":/root/cometclient/ || { echo "Failed to copy main.py file to $container"; exit 1; }
+#    docker exec "$container" bash -c "cd /root/abci && /usr/local/go/bin/go clean -modcache && /usr/local/go/bin/go mod tidy && /usr/local/go/bin/go build -o /root/abci-app *.go"
+#    if (( i < 12 )); then
+#     docker exec -d "$container" bash -c "cd /root/abci/clusterAConfig && nohup /root/abci-app > /root/logs/abci.log 2>&1"
+#    else
+#      docker exec -d "$container" bash -c "cd /root/abci/clusterBConfig && nohup /root/abci-app > /root/logs/abci.log 2>&1"
+#    fi
+#    sleep 2
 
-    echo "[7] Restarting CometBFT..."
-    docker exec "$container" rm -f /root/.cometbft/config/genesis.json
-    if (( i < 12 )); then
-      docker cp "./cluster1Config/genesis.json" "$container":/root/.cometbft/config/
-    else
-      docker cp "./cluster2Config/genesis.json" "$container":/root/.cometbft/config/
-    fi
-    nodeId=$(docker exec "$container" /root/go/bin/cometbft show-node-id)
-    docker exec "$container" curl -i -X POST -H "Content-Type: application/json" -d "{\"tags\":{\"rpc_addr\":\"$nodeId@$ip_address:26656\"}}" http://127.0.0.1:5555/updatetags
-    docker exec -d "$container" bash -c "nohup /root/go/bin/cometbft node > /root/logs/cometbft.log 2>&1"
-    sleep 3
+#    echo "[7] Restarting CometBFT..."
+#    docker exec "$container" rm -f /root/.cometbft/config/genesis.json
+#    if (( i < 12 )); then
+#      docker cp "./cluster1Config/genesis.json" "$container":/root/.cometbft/config/
+#    else
+#      docker cp "./cluster2Config/genesis.json" "$container":/root/.cometbft/config/
+#    fi
+#    nodeId=$(docker exec "$container" /root/go/bin/cometbft show-node-id)
+#    docker exec "$container" curl -i -X POST -H "Content-Type: application/json" -d "{\"tags\":{\"rpc_addr\":\"$nodeId@$ip_address:26656\"}}" http://127.0.0.1:5555/updatetags
+#    docker exec -d "$container" bash -c "nohup /root/go/bin/cometbft node > /root/logs/cometbft.log 2>&1"
+#    sleep 3
 
     echo "[8] Verifying logs..."
     docker exec "$container" tail -n 20 /root/logs/abci.log
